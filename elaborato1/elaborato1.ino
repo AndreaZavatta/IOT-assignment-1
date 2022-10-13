cbf3dfd523a2454fbc32c8379dc5ebec71303914
Luca Pasini <pasini.luca09@gmail.com>
2022-10-13 11:55:50 +0200

    Add sequence generator and add header file

--- elaborato1/elaborato1.ino
+++ elaborato1/elaborato1.ino
@@ -2,6 +2,7 @@
 //#define EI_ARDUINO_INTERRUPTED_PIN  // to enable pin states functionality
 #include <EnableInterrupt.h>
 #include <avr/sleep.h>
+#include"./header.h"
 #define BTN_BLUE 11
 #define BTN_GREEN 10
 #define BTN_ORANGE 9
@@ -21,8 +22,10 @@ int brightness = 0;
 int fadeamount = 5;
 bool gamestart = false;
 long prevts = 0;
-int[4] generated;
-int[4] clicked;
+int generated[4];
+int T1 = 2000;
+int T2 = 5000;
+int T3 = 10000;
 
 int life;
 int points;
@@ -45,69 +48,62 @@ void sleep() {
   sleep_disable();
 }
 
-void setup() {
-  life = 3;
-  points = 0;
-  sleeping = false;
-  pinMode(LED_YELLOW, OUTPUT);
-  pinMode(LED_ORANGE, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(BTN_YELLOW, INPUT);
-  pinMode(BTN_ORANGE, INPUT);
-  pinMode(BTN_GREEN, INPUT);
-  pinMode(BTN_BLUE, INPUT);
-
-  pinMode(A5, OUTPUT);
-
-  enableInterrupt(BTN_YELLOW, changeSleep, CHANGE);
-  enableInterrupt(BTN_ORANGE, changeSleep, CHANGE);
-  enableInterrupt(BTN_GREEN, changeSleep, CHANGE);
-
-  enableInterrupt(BTN_BLUE, game, CHANGE);
-  //questo è solo per scopo illustrativo!!!! disabiliti un pin, serve per riusare i bottoni durante il gioco e nella fase iniziale
-  disableInterrupt(BTN_GREEN);
-
-  Serial.begin(PORT);
-  pinMode(LED_WHITE, OUTPUT);
-  func();
-  digitalWrite(LED_WHITE, LOW);
-  //read potenziometro.
-  difficulty = analogRead(A5);
-  Serial.println(difficulty);
-  Serial.println("difficlolta printata");
-  disableAllInterrupts();
+int checkLed(int led) {
+  for (int i = 0; i < 4; i++) {
+    if (generated[i] == led){
+      return i;
+    }
+  return -1;
+  }
 }
 
-void pressGreen(){
-  presspin(BTN_GREEN);
+bool checkWin() {
+  for (int i = 0; i < 4; i++) {
+    if (generated[i] != 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-void pressYellow(){
-  presspin(BTN_YELLOW);
-}
-
-void pressOrange(){
-  presspin(BTN_ORANGE);
-}
-
-void pressBlue(){
-  presspin(BTN_BLUE);
-}
-
-void presspin(int pin) {
+void pressBtn(int led) {
   long ts = micros();
   if (ts - prevts > 200000) {
-    Serial.println(pin);
+    int i = checkLed(led);
+    if (i != -1) {
+      generated[i] = 0;
+      digitalWrite(led, HIGH);
+      if (checkWin()) {
+        Serial.println("Win");
+        points ++;
+      }
+    }
+
     prevts = ts;
   }
 }
 
-void loop() {
-  enableInterrupt(BTN_GREEN, pressGreen, CHANGE);
-  enableInterrupt(BTN_YELLOW, pressYellow, CHANGE);
-  enableInterrupt(BTN_BLUE, pressBlue, CHANGE);
-  enableInterrupt(BTN_ORANGE, pressOrange, CHANGE);
+void pressGreen(){
+  pressBtn(LED_GREEN);
+}
+
+void pressYellow(){
+  pressBtn(LED_YELLOW);
+}
+
+void pressOrange(){
+  pressBtn(LED_ORANGE);
+}
+
+void pressBlue(){
+  pressBtn(LED_BLUE);
+}
+
+void lightOut() {
+  digitalWrite(LED_BLUE, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_ORANGE, LOW);
+  digitalWrite(LED_YELLOW, LOW);
 }
 
 void fading() {
@@ -149,16 +145,65 @@ void game() {
   }
 }
 
-void randomSeq() {
+int randomSeq() {
   int num = random(1,5);
   for (int i = 0; i < num; i++) {
     generated[i] = random(LED_YELLOW, LED_BLUE + 1);
   }
+  return num;
 }
 
 void resetSeq() {
   for (int i = 0; i < 4; i++) {
     generated[i] = 0;
-    clicked[i] = 0;
   }
+}
+
+void setup() {
+  life = 3;
+  points = 0;
+  sleeping = false;
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_ORANGE, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(BTN_YELLOW, INPUT);
+  pinMode(BTN_ORANGE, INPUT);
+  pinMode(BTN_GREEN, INPUT);
+  pinMode(BTN_BLUE, INPUT);
+
+  pinMode(A5, OUTPUT);
+
+  enableInterrupt(BTN_YELLOW, changeSleep, CHANGE);
+  enableInterrupt(BTN_ORANGE, changeSleep, CHANGE);
+  enableInterrupt(BTN_GREEN, changeSleep, CHANGE);
+
+  enableInterrupt(BTN_BLUE, game, CHANGE);
+  //questo è solo per scopo illustrativo!!!! disabiliti un pin, serve per riusare i bottoni durante il gioco e nella fase iniziale
+  disableInterrupt(BTN_GREEN);
+
+  Serial.begin(PORT);
+  pinMode(LED_WHITE, OUTPUT);
+  func();
+  digitalWrite(LED_WHITE, LOW);
+  //read potenziometro.
+  difficulty = analogRead(A5);
+  Serial.println(difficulty);
+  Serial.println("difficlolta printata");
+  disableAllInterrupts();
+  enableInterrupt(BTN_GREEN, pressGreen, CHANGE);
+  enableInterrupt(BTN_YELLOW, pressYellow, CHANGE);
+  enableInterrupt(BTN_BLUE, pressBlue, CHANGE);
+  enableInterrupt(BTN_ORANGE, pressOrange, CHANGE);
+  resetSeq();
+}
+
+void loop() {
+  delay(T1);
+  int num = randomSeq();
+  for ( int i = 0; i < num; i++) {
+    digitalWrite(generated[i], HIGH);
+  }
+  delay(T2);
+  lightOut();
 }
---
+++ elaborato1/header.h
@@ -0,0 +1,31 @@
+void disableAllInterrupts();
+
+void sleep();
+
+int checkLed(int led);
+
+bool checkWin();
+
+void pressBtn(int led);
+
+void pressGreen();
+
+void pressYellow();
+
+void pressOrange();
+
+void pressBlue();
+
+void lightOut();
+
+void fading();
+
+void func();
+
+void changeSleep();
+
+void game();
+
+int randomSeq();
+
+void resetSeq();
